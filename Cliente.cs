e6d46a5639e4483b31c3bc5d1c4b70d52b673632
Erick Goldberg <erick_goldberg@hotmail.com>
2023-06-29 09:15:23 -0300

    first commit

---
+++ Cliente.cs
@@ -0,0 +1,28 @@
+﻿namespace Teste
+{
+    class Cliente
+    {
+        private int _NumeroDaConta;
+        private string _Titular;
+        private int _Deposito = 0;
+
+        public int NumeroDaConta { get => _NumeroDaConta; set => _NumeroDaConta = value; }
+        public string Titular { get => _Titular; set => _Titular = value; }
+        public int Deposito { get => _Deposito; set => _Deposito = value; }
+
+        public void Depositar(int deposito)
+        {
+            _Deposito += deposito;
+        }
+
+        public void Sacar(int saque)
+        {
+            _Deposito -= (saque + 5);
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + NumeroDaConta + ", Titular " + Titular + ", Saldo " + Deposito;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+using Teste;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cliente cliente1 = new Cliente();
+
+        Console.Write("Entre o número da conta: ");
+        cliente1.NumeroDaConta = int.Parse(Console.ReadLine());
+
+        Console.Write("Entre o titular da conta: ");
+        cliente1.Titular = Console.ReadLine();
+
+        Console.Write("Haverá deposito inicial (s/n)? ");
+        char escolha = char.Parse(Console.ReadLine());
+        if (escolha == 's')
+        {
+            Console.Write("Entre o valor do deposito inicial: ");
+            cliente1.Deposito = int.Parse(Console.ReadLine());
+        }
+        
+        Console.WriteLine("Dados da conta:");
+        Console.Write(cliente1 + "\n \n");
+
+        Console.Write("Entre um valor para déposito: ");
+        int valorConta = int.Parse(Console.ReadLine());
+        cliente1.Depositar(valorConta);
+
+        Console.WriteLine("Dados da conta atualizados: ");
+        Console.Write(cliente1 + "\n \n");
+
+        Console.Write("Entre um valor para saque: ");
+        valorConta = int.Parse(Console.ReadLine());
+        cliente1.Sacar(valorConta);
+
+        Console.WriteLine("Dados da conta atualizados: ");
+        Console.Write(cliente1 + "\n \n");
+
+    }
+}
